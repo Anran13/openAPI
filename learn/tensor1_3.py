b65bcaea7fd20bab4fda3a962c22bbfaa51f24cd
Anran13 <s0983866872@gmail.com>
2025-08-14 13:12:15 +0000

    modify

---
+++ learn/tensor1_3.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+def load_and_get_tflite_model(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    def predict(x):
+        interpreter.set_tensor(input_details[0]['index'], np.array([[x]], dtype=np.float32).reshape(input_details[0]['shape']))
+        interpreter.invoke()
+        return interpreter.get_tensor(output_details[0]['index'])   
+    
+    return predict
+
+
+st.set_page_config(layout="centered")
+
+st.title("TensorFlow Lite Demo")
+
+prompt = st.chat_input("Please input x value:")
+if prompt:
+    input_value = float(prompt)
+    tflite_model_path = 'linear_model.tflite'
+    predict = load_and_get_tflite_model(tflite_model_path)
+    st.write(predict(input_value))
